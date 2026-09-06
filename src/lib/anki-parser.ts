687f3f113ee9d7a1d55566e0a15b335be2d7ba08
SDX1023 <si.davidsdx@gmail.com>
2026-09-06 19:42:47 +0800

    fix: use correct sql.js WASM filename sql-wasm-browser.wasm

--- src/lib/anki-parser.ts
+++ src/lib/anki-parser.ts
@@ -22,7 +22,7 @@ export interface ParsedAnkiDeck {
 let sqlPromise: ReturnType<typeof initSqlJs> | null = null;
 
 async function getSQL() {
-  if (!sqlPromise) sqlPromise = initSqlJs({ locateFile: (file: string) => `/${file}` });
+  if (!sqlPromise) sqlPromise = initSqlJs({ locateFile: () => "/sql-wasm-browser.wasm" });
   return sqlPromise;
 }
 
